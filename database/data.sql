d3e452e8b41cd365b7e90529c972ed5ef7a6d260
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2024-02-07 01:35:47 +0000

    corrected data.sql syntax

--- database/data.sql
+++ database/data.sql
@@ -18,11 +18,11 @@ insert into "dailyQuizzes"
 insert into "dailyQuizQuestions"
 ("dailyQuizId", "question", "difficulty")
 values
-(1, 'Who was the top scorer of the 2014 FIFA World Cup?', 'Medium')
-(1, 'In 2016, who won the Formula 1 World Constructor''s Championship for the third time in a row?', 'Medium')
-(1, 'Which player holds the NHL record of 2,857 points?', 'Easy')
-(1, 'Which country did Kabaddi, a contact sport involving tackling, originate from?', 'Medium')
-(1, 'In baseball, how many fouls are an out?', 'Easy')
+(1, 'Who was the top scorer of the 2014 FIFA World Cup?', 'Medium'),
+(1, 'In 2016, who won the Formula 1 World Constructor''s Championship for the third time in a row?', 'Medium'),
+(1, 'Which player holds the NHL record of 2,857 points?', 'Easy'),
+(1, 'Which country did Kabaddi, a contact sport involving tackling, originate from?', 'Medium'),
+(1, 'In baseball, how many fouls are an out?', 'Easy');
 
 insert into "dailyQuizAnswers"
 ("dailyQuestionId", "answer", "isCorrect")
@@ -30,19 +30,19 @@ values
 (1, 'Thomas Müller', false),
 (1, 'Lionel Messi', false),
 (1, 'Neymar', false),
-(1, 'James Rodríguez', true);
+(1, 'James Rodríguez', true),
 (2, 'Scuderia Ferrari', false),
 (2, 'McLaren Honda', false),
 (2, 'Red Bull Racing Renault', false),
-(2, 'Mercedes-AMG Petronas', true);
+(2, 'Mercedes-AMG Petronas', true),
 (3, 'Mario Lemieux', false),
 (3, 'Sidney Crosby', false),
 (3, 'Gordie Howe', false),
-(3, 'Wayne Gretzky', true);
+(3, 'Wayne Gretzky', true),
 (4, 'Australia', false),
 (4, 'Turkey', false),
 (4, 'Cambodia', false),
-(4, 'India', true);
+(4, 'India', true),
 (5, '5', false),
 (5, '3', false),
 (5, '2', false),
